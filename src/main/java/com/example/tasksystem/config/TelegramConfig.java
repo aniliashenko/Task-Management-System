d51a0a62e177e0372b25a3e98e886e54bd89820e
azura <windsweptofficial@gmail.com>
2025-12-02 17:37:32 +0100

    fixed test issue-4

--- src/main/java/com/example/tasksystem/config/TelegramConfig.java
+++ src/main/java/com/example/tasksystem/config/TelegramConfig.java
@@ -2,10 +2,12 @@ package com.example.tasksystem.config;
 
 import com.example.tasksystem.telegram.service.TelegramBot;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@Profile("!test")
 public class TelegramConfig {
 
     public TelegramConfig(TelegramBot bot) throws Exception {
